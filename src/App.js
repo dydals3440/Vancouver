46c25c0b9c48dede052bdc710ff186025600ece0
김용민 <dydals3440@gmail.com>
2023-05-27 20:25:56 +0900

    feat: Page별 Routing 추가 및 index에서 페이지 관리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
-
+import { Outlet } from 'react-router-dom';
+// index.jsx를 통해 파일을 조금 더 깔끔하게 관리함, 직접 컴포넌트 파일로 바로 연결안해도됨.(인덱스
 function App() {
   return (
-    <div>
-      <h4>Vancouver</h4>
-    </div>
+    <>
+      <Navbar />
+      <Sidebar />
+      <Outlet />
+      <Footer />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -9,6 +10,51 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  CheckOut,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/products',
+        element: <Products />,
+      },
+      {
+        path: '/products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: '/checkout',
+        element: <CheckOut />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
