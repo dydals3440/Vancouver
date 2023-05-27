49d6a0559a2d596ce2970398b7e8ed40d6b30e6f
김용민 <dydals3440@gmail.com>
2023-05-27 23:53:19 +0900

    feat: error page (잘못된 접근시, 홈페이지로 돌아갈 수 있게 설정)

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Navbar, Sidebar } from '../components';
 const ErrorPage = () => {
-  return <h4>error page</h4>;
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Wrapper className='page-100'>
+        <section>
+          <h1>404</h1>
+          <h3>
+            죄송합니다. <br />
+            현재 접근이 불가능한 페이지를 요청하셨습니다.
+          </h3>
+          <Link to='/' className='btn'>
+            홈페이지로 돌아가기
+          </Link>
+        </section>
+      </Wrapper>
+    </>
+  );
 };
 
 const Wrapper = styled.main`
