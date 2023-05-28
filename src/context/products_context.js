49701c568c5b4cd40a05385cfd818c4a0de07231
김용민 <dydals3440@gmail.com>
2023-05-28 16:24:02 +0900

    feat: featured products using Redux dispath&payload

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,12 @@ import {
 
 const initialState = {
   isSidebarOpen: true,
+  // 단일 제품 로딩
+  products_loading: false,
+  products_error: false,
+  // 모든게 제대로 되면 실제 제품을 받아옴
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = createContext();
@@ -28,6 +34,23 @@ export const ProductsProvider = ({ children }) => {
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
+
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      // 해당 타입 액션을 dispatch하고, payload에 전달된 products 데이터를 활용하여 상태를 업데이트!
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
+
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
