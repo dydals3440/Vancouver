65ecd1ee720f19cbe4250116262e21be46d144f9
김용민 <dydals3440@gmail.com>
2023-05-29 02:45:17 +0900

    feat: Star Components (동적으로, Array.from 메소드 활용하여, Stars 채우기 구현

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -2,66 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 const Stars = ({ stars, reviews }) => {
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    // index 0 - 4
+    const number = index + 0.5;
+    return (
+      <span>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
+  console.log(tempStars);
   console.log(stars, reviews);
   return (
     <Wrapper>
-      <div className='stars'>
-        {/* star */}
-        <span>
-          {stars >= 1 ? (
-            <BsStarFill />
-          ) : stars >= 0.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 1.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-      </div>
+      <div className='stars'>{tempStars}</div>
       <p className='reviews'>({reviews}개의 고객 리뷰)</p>
     </Wrapper>
   );
@@ -82,3 +42,50 @@ const Wrapper = styled.div`
   margin-bottom: 0.5rem;
 `;
 export default Stars;
+
+// 수동적으로 별 생성하는 코드(로직 이해 중요!)
+//  {
+//    /* star */
+//  }
+//  <span>
+//    {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}
+//  </span>;
+//  {
+//    /* end of star */
+//  }
+//  {
+//    /* star */
+//  }
+//  <span>
+//    {stars >= 2 ? <BsStarFill /> : stars >= 1.5 ? <BsStarHalf /> : <BsStar />}
+//  </span>;
+//  {
+//    /* end of star */
+//  }
+//  {
+//    /* star */
+//  }
+//  <span>
+//    {stars >= 3 ? <BsStarFill /> : stars >= 2.5 ? <BsStarHalf /> : <BsStar />}
+//  </span>;
+//  {
+//    /* end of star */
+//  }
+//  {
+//    /* star */
+//  }
+//  <span>
+//    {stars >= 4 ? <BsStarFill /> : stars >= 3.5 ? <BsStarHalf /> : <BsStar />}
+//  </span>;
+//  {
+//    /* end of star */
+//  }
+//  {
+//    /* star */
+//  }
+//  <span>
+//    {stars >= 5 ? <BsStarFill /> : stars >= 4.5 ? <BsStarHalf /> : <BsStar />}
+//  </span>;
+//  {
+//    /* end of star */
+//  }
