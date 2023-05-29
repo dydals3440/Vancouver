f14068ad56964d4943aad66bd0d18f49fe7d466a
김용민 <dydals3440@gmail.com>
2023-05-29 21:40:45 +0900

    feat: 객체의 값 접근, 객체 안의 배열 값에 접근 할 때 flat을 통한 접근

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,4 +6,13 @@ export const formatPrice = (num) => {
   return newNumber;
 };
 
-export const getUniqueValues = () => {};
+// 카테고리 별, 데이터들을 새로운 배열로 만들어 제공
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  // company나 category는 객체 안에 들어있어서 바로 접근이 가능하지만, colors같은경우는, 객체안의 colors 배열 안에 값들이 있어서 flat을 통해 접근해야한다.
+  // ex. const arr = [1, 2, [3, 4]] => arr.flat() = [1, 2, 3, 4]
+  if (type == 'colors') {
+    unique = unique.flat();
+  }
+  return ['all', ...new Set(unique)];
+};
